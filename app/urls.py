31f03f859173a4cc6187873f8e8fc080f134af25
dmitry <dkoshenkofff@mail.ru>
2024-04-01 00:01:01 +0300

    without pagination

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,14 @@
-
 from django.urls import path
 from app import views
 
-urlpatterns = [
-    path('', views.index, name='index')
-]
+urlpatterns = {
+    path('', views.index, name='index'),
+    path('hot', views.hot, name='hot'),
+    path('question/<int:question_id>', views.questions, name='question'),
+    path('ask', views.ask, name='ask'),
+    path('signup', views.sign_up, name='sign_up'),
+    path('login', views.login, name='login'),
+    path('settings', views.settings, name='settings'),
+    path('tag/<str:tag_name>', views.tag, name='tag'),
+    path('logout', views.logout, name='logout'),
+}
--- app/views.py
+++ app/views.py
@@ -1,15 +1,69 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
-QUESTION = [
+QUESTIONS = [
     {
         "title": f"Question {i}",
-        "text": f"This is the {i} question",
-    } for i in range(10)
+        "text": f"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore "
+                f"et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
+                f"aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse "
+                f"cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, "
+                f"sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "tags": ["tag1", "tag2", "tag3"],
+    } for i in range(200)
+]
+
+ANSWERS = [
+    {
+        "text": f"{i} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porttitor est diam, "
+                f"vel pellentesque sapien molestie non. Suspendisse pretium augue."
+    } for i in range(2)
 ]
 
 
 def index(request):
-    return render(request, "index.html", {"questions": QUESTION})
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(QUESTIONS, 5)
+    page_obj = paginator.page(page_num)
 
+    return render(request, "index.html", {"questions": QUESTIONS})
+
+
+def hot(request):
+    return render(request, "hot.html", {"questions": QUESTIONS[::-1]})
+
+
+def questions(request, question_id):
+    item = QUESTION[question_id]
+    return render(request, "question.html", {"question": item, "answers": ANSWERS})
+
+
+def ask(request):
+    return render(request, "ask.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def sign_up(request):
+    return render(request, "signup.html")
+
+
+def tag(request, tag_name):
+    for q in QUESTION:
+        q["tags"] = [tag_name]
+    return render(request, "tag.html", {"tag_name": tag_name, "questions": QUESTIONS[::4]})
+
+
+def settings(request):
+    return render(request, "settings.html")
+
+
+def logout(request):
+    return redirect("/login", permanent=True)
+
+def pagination():
+    pass
