c6497604e6e2df1f130adcb6158390ada9d1056e
dmitry <dkoshenkofff@mail.ru>
2024-04-01 00:50:49 +0300

    pagination done

--- app/urls.py
+++ app/urls.py
@@ -4,11 +4,11 @@ from app import views
 urlpatterns = {
     path('', views.index, name='index'),
     path('hot', views.hot, name='hot'),
-    path('question/<int:question_id>', views.questions, name='question'),
+    path('question/<int:question_id>', views.question, name='question'),
     path('ask', views.ask, name='ask'),
     path('signup', views.sign_up, name='sign_up'),
     path('login', views.login, name='login'),
     path('settings', views.settings, name='settings'),
     path('tag/<str:tag_name>', views.tag, name='tag'),
-    path('logout', views.logout, name='logout'),
+    path('logout', views.logout_view, name='login'),
 }
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import logout
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 
@@ -7,10 +8,7 @@ QUESTIONS = [
     {
         "title": f"Question {i}",
         "text": f"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore "
-                f"et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-                f"aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse "
-                f"cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, "
-                f"sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                f"et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ",
         "tags": ["tag1", "tag2", "tag3"],
     } for i in range(200)
 ]
@@ -19,25 +17,31 @@ ANSWERS = [
     {
         "text": f"{i} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porttitor est diam, "
                 f"vel pellentesque sapien molestie non. Suspendisse pretium augue."
-    } for i in range(2)
+    } for i in range(20)
 ]
 
 
-def index(request):
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 5)
-    page_obj = paginator.page(page_num)
+def pagination(request, items, count=3):
+    page_num = int(request.GET.get('page', 1))
+    paginator = Paginator(items, count)
+    page_obj = paginator.get_page(page_num)
+    return paginator, page_obj, page_num
 
-    return render(request, "index.html", {"questions": QUESTIONS})
+
+def index(request):
+    paginator, questions, page_num = pagination(request, QUESTIONS)
+    return render(request, "index.html", {"questions": questions, "paginator": paginator})
 
 
 def hot(request):
-    return render(request, "hot.html", {"questions": QUESTIONS[::-1]})
+    paginator, items, page_num = pagination(request, QUESTIONS)
+    return render(request, "hot.html", {"items": items[::-1], "paginator": paginator})
 
 
-def questions(request, question_id):
-    item = QUESTION[question_id]
-    return render(request, "question.html", {"question": item, "answers": ANSWERS})
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    paginator, items, page_num = pagination(request, ANSWERS, 2)
+    return render(request, "question.html", {"question": item, "answers": items, "paginator": pagination(request, ANSWERS, 2)})
 
 
 def ask(request):
@@ -53,17 +57,17 @@ def sign_up(request):
 
 
 def tag(request, tag_name):
-    for q in QUESTION:
+    Q = QUESTIONS.copy()
+    for q in Q:
         q["tags"] = [tag_name]
-    return render(request, "tag.html", {"tag_name": tag_name, "questions": QUESTIONS[::4]})
+    paginator, items, page_num = pagination(request, QUESTIONS)
+    return render(request, "index.html", {"items": items, "paginator": paginator})
 
 
 def settings(request):
     return render(request, "settings.html")
 
 
-def logout(request):
+def logout_view(request):
+    logout(request)
     return redirect("/login", permanent=True)
-
-def pagination():
-    pass
