31ecc162b14fd0b11dac907a295359ed023587d7
dmitry <dkoshenkofff@mail.ru>
2024-03-31 20:19:18 +0300

    add templates in index

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+QUESTION = [
+    {
+        "title": f"Question {i}",
+        "text": f"This is the {i} question",
+    } for i in range(10)
+]
+
+
+def index(request):
+    return render(request, "index.html", {"questions": QUESTION})
+
--- askme_koshenkov/settings.py
+++ askme_koshenkov/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'askme_koshenkov.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "var/www/static",
+]
